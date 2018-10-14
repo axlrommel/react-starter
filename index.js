98b653046f1f8525316dedb2144b5b916bb16f3e
Rommel Villagomez <rommelvillagomez@rommels-air.ymcastlouis.org>
2018-10-14 15:11:12 -0500

    remove unnecessary files

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let fs = require('fs');
+let path = require('path');
+
+exports.get = function (event, context, callback) {
+  let contents = fs.readFileSync(`public${path.sep}index.html`);
+  let result = {
+    statusCode: 200,
+    body: contents.toString(),
+    headers: { 'content-type': 'text/html' }
+  };
+
+  callback(null, result);
+};
