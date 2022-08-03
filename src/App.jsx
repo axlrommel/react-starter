a4fbd1045f51b4c14d219cdf65293fb6e426407d
axlrommel <rommelvillagomez77@gmail.com>
2022-08-03 13:42:11 -0500

    starter code only

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "./theme";
-import AppContainer from "./Music";
+import AppContainer from "./AppContainer";
 
 function App() {
   return (
--- src/components/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const App = () => (
-  <div>
-    <h4>Hello World!</h4>
-  </div>
-);
-
-export default App;
